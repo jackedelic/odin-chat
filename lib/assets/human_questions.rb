12121ea3564d686ba615f0c1a04ba2fd853cd0a4
Jack Wong <jackwong3101@yahoo.com>
2017-08-04 19:35:20 +0800

    bot updated

--- lib/assets/human_questions.rb
+++ lib/assets/human_questions.rb
@@ -3,22 +3,22 @@
 		"who are you","who r you","who r u","tell me about yourself","who r u"
 	],
 	"hi":[
-		"hi","hello","^\s*yo(|!|\s)"
+		"hi","hello","^\s*yo(|!+|\s)"
 	],
 	"ok":[
 		"ok","alright","orite","okay","okie"
 	],
 	"thanks":[
-		"thanks","thank [(you)(u)]"
+		"thanks","thank (you)|(u)"
 	],	
 	"i am bored":[
-		"i [(am)(feel)] bored","this is boring"
+		"i (am)|(feel) bored","this is boring"
 	],
 	"i am lonely":[
-		"i [(am)(feel)] lonely"
+		"i (am)|(feel) lonely"
 	],
 	"creator":[
-		"who create[d]? [(you)(u)]","where [(are)(r)] [(you)(u)] from"
+		"who create[d]? (you)|(u)","where (are)|(r) (you)|(u) from"
 	]
 
 }
