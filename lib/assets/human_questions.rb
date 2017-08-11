e4229ad6f0a53bdd8ce9016b3b6a9ae6cd0302ca
Jack Wong <jackwong3101@yahoo.com>
2017-08-11 09:07:38 +0800

    .

--- lib/assets/human_questions.rb
+++ lib/assets/human_questions.rb
@@ -1,24 +1,24 @@
 {
 	"who r u":[
-		"who are you","who r you","who r u","tell me about yourself","who r u"
+		"who (are|r) (you|u)","tell me about (yourself|urself)"
 	],
 	"hi":[
 		"hi[^a-z]*","[^a-z]*hello[^a-z]*","^\s*yo[^a-z]"
 	],
 	"ok":[
-		"[^a-z]*ok[^a-z]*","alright","orite","[^a-z]*ok(ay)|(ie)[^a-z]*"
+		"([^a-z]+|^)ok([^a-z]|$)","alright","orite","([^a-z]+|^)ok(ay)|(ie)([^a-z]|$)"
 	],
 	"thanks":[
-		"thanks","thank (you)|(u)"
+		"thanks","thank (you|u)"
 	],	
 	"i am bored":[
-		"i (am)|(feel) bored","this is boring"
+		"i (am|feel) bored","this is boring"
 	],
 	"i am lonely":[
-		"i (am)|(feel) lonely"
+		"i (am|feel) lonely"
 	],
 	"creator":[
-		"who create[d]? (you)|(u)","where (are)|(r) (you)|(u) from"
+		"who create[d]? (you|u)","where (are|r) (you|u) from"
 	]
 
 }
